5d9a6a36ad802e742c5e42eea3a8b0d06e966b4a
Vinicius Moreira <vinicius.moreira.silva557@gmail.com>
2024-06-24 19:12:08 -0300

    finish

--- index.js
+++ index.js
@@ -20,6 +20,191 @@ app.get("/", (req, res) => {
     res.render("index", {});
 });
 
+app.get("/agendamentos", async (req, res) => {
+    try {
+        const getAgendamentos = new Promise((resolve, reject) => {
+            database.query("SELECT * FROM agendamentos", (error, results, fields) => {
+                if (error) {
+                    console.log(`Error while querying the database: ${error}`);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        const agendamentos = await getAgendamentos;
+
+        const getClienteById = (clienteId) => {
+            return new Promise((resolve, reject) => {
+                database.query("SELECT * FROM clientes WHERE id = ?", [clienteId], (error, results, fields) => {
+                    if (error) {
+                        console.log(`Error while querying the database: ${error}`);
+                        reject(error);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+            });
+        };
+
+        const getBarbeiroById = (barbeiroId) => {
+            return new Promise((resolve, reject) => {
+                database.query("SELECT * FROM barbeiros WHERE id = ?", [barbeiroId], (error, results, fields) => {
+                    if (error) {
+                        console.log(`Error while querying the database: ${error}`);
+                        reject(error);
+                    } else {
+                        resolve(results[0]);
+                    }
+                });
+            });
+        };
+
+        const agendamentosFull = await Promise.all(
+            agendamentos.map(async (agendamento) => {
+                const cliente = await getClienteById(agendamento.clienteId);
+                const barbeiro = await getBarbeiroById(agendamento.barbeiroId);
+                return { 
+                    ...agendamento, 
+                    cliente, 
+                    barbeiro 
+                };
+            })
+        );
+
+        res.render("agendamentos", { agendamentos: agendamentosFull });
+    } catch (error) {
+        console.log(`Error while querying the database: ${error}`);
+        res.render("agendamentos", { agendamentos: [] });
+    }
+});
+
+app.get("/agendamentos/:id/edit", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const getAgendamento = new Promise((resolve, reject) => {
+            database.query(`SELECT * FROM agendamentos WHERE id = ${id}`, (error, results, fields) => {
+                if (error) {
+                    console.log(`Error while querying the database: ${error}`);
+                    reject(error);
+                } else {
+                    resolve(results[0]);
+                }
+            });
+        });
+
+        const getClientes = new Promise((resolve, reject) => {
+            database.query("SELECT * FROM clientes", (error, results, fields) => {
+                if (error) {
+                    console.log(`Error while querying the database: ${error}`);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        const getBarbeiros = new Promise((resolve, reject) => {
+            database.query("SELECT * FROM barbeiros", (error, results, fields) => {
+                if (error) {
+                    console.log(`Error while querying the database: ${error}`);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        const [agendamento, clientes, barbeiros] = await Promise.all([getAgendamento, getClientes, getBarbeiros]);
+
+        res.render("agendamentoEdit", { agendamento, clientes, barbeiros });
+    } catch (error) {
+        console.log(`Error while querying the database: ${error}`);
+        res.render("agendamentoEdit", { agendamento: {}, clientes: [], barbeiros: [] });
+    }
+});
+
+app.get("/agendamentos/add", async (req, res) => {
+    try {
+        const getClientes = new Promise((resolve, reject) => {
+            database.query("SELECT * FROM clientes", (error, results, fields) => {
+                if (error) {
+                    console.log(`Error while querying the database: ${error}`);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        const getBarbeiros = new Promise((resolve, reject) => {
+            database.query("SELECT * FROM barbeiros", (error, results, fields) => {
+                if (error) {
+                    console.log(`Error while querying the database: ${error}`);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+
+        const [clientes, barbeiros] = await Promise.all([getClientes, getBarbeiros]);
+
+        res.render("agendamentoAdd", { clientes, barbeiros });
+    } catch (error) {
+        console.log(`Error while querying the database: ${error}`);
+        res.render("agendamentoAdd", { clientes: [], barbeiros: [] });
+    }
+});
+
+app.post("/agendamentos", (req, res) => {
+    const { clienteId, barbeiroId, data, hora } = req.body;
+    try {
+        database.query(`INSERT agendamentos (clienteId, barbeiroId, data, hora) VALUES ('${clienteId}', '${barbeiroId}', '${data}', '${hora}')`, (error, results, fields) => {
+            if (error) {
+                console.log(`Error while querying the database: ${error}`);
+            } else {
+                res.redirect("/agendamentos");
+            }
+        });
+    } catch (error) {
+        console.log(`Error while querying the database: ${error}`);
+        res.redirect("/agendamentos");
+    }
+});
+
+app.post("/agendamentos/:id", (req, res) => {
+    const { clienteId, barbeiroId, data, hora } = req.body;
+    try {
+        database.query(`UPDATE agendamentos SET clienteId = '${clienteId}', barbeiroId = '${barbeiroId}', data = '${data}', hora = '${hora}' WHERE id = ${req.params.id}`, (error, results, fields) => {
+            if (error) {
+                console.log(`Error while querying the database: ${error}`);
+            } else {
+                res.redirect("/agendamentos");
+            }
+        });
+    } catch (error) {
+        console.log(`Error while querying the database: ${error}`);
+        res.redirect("/agendamentos");
+    }
+});
+
+app.delete("/agendamentos/:id", (req, res) => {
+    try {
+        database.query(`DELETE FROM agendamentos WHERE id = ${req.params.id}`, (error, results, fields) => {
+            if (error) {
+                console.log(`Error while querying the database: ${error}`);
+            } else {
+                res.redirect("/agendamentos");
+            }
+        });
+    } catch (error) {
+        console.log(`Error while querying the database: ${error}`);
+        res.redirect("/agendamentos");
+    }
+});
+
 app.get("/clientes", (req, res) => {
     try {
         database.query("SELECT * FROM clientes", (error, results, fields) => {
